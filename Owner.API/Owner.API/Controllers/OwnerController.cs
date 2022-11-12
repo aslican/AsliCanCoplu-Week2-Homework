7b594553576ff6c87060081d5375f00193c4b390
aslican <aslican.coplu@gmail.com>
2022-11-12 18:09:26 +0300

    homework2 is done

---
+++ Owner.API/Owner.API/Controllers/OwnerController.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Owner.API.Model;
+
+namespace Owner.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OwnerController : ControllerBase
+    {
+        List<OwnerModel> ownerList = new List<OwnerModel>()
+        {
+            new OwnerModel
+            {
+                Id = 1,
+                Name = "Aslı Can",
+                Surname = "Çöplü",
+                Date = DateTime.Now,
+                Statement ="Student of Bootcamp",
+                Type = "Student",
+
+            },
+            new OwnerModel
+            {
+                Id = 2,
+                Name = "Doğuhan",
+                Surname = "Altın",
+                Date = DateTime.Now,
+                Statement ="!(Student of Bootcamp)",
+                Type = "Student",
+
+            },
+            new OwnerModel
+            {
+                Id = 3,
+                Name = "gökhan",
+                Surname = "Civelek",
+                Date = DateTime.Now,
+                Statement ="!(Student of Bootcamp)",
+                Type = "part-time student",
+
+            },
+           
+        }; 
+        [HttpGet]
+        [Route("GetAllOwners")]
+        public IActionResult GetAllOwners()
+        {
+            return Ok(ownerList);
+        }
+
+        [HttpPost]
+        [Route("AddOwner")]
+        public IActionResult AddOwner([FromBody]OwnerModel newOwner)
+        {
+            
+            if (newOwner.Statement.Contains("hack"))
+            {
+                throw new Exception();
+            }
+            ownerList.Add(newOwner);
+            return Ok(ownerList);   
+        }
+        [HttpDelete("{id}")]
+        
+        public IActionResult DeleteOwner(int id)
+        {
+            var deletedOwner = ownerList.FirstOrDefault(x => x.Id == id);
+            if (deletedOwner == null)
+            {
+                throw new Exception($"{id} owner is not found");
+            }
+
+            ownerList.Remove(deletedOwner);
+            return Ok(ownerList);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateOwners([FromBody] OwnerModel updatedOwner,int id)
+        {
+            if(id != updatedOwner.Id)
+            {
+                throw new Exception("owner id cannot be changed");
+
+            }
+            var changeOwner = ownerList.FirstOrDefault(x => x.Id == id);
+            if(changeOwner == null)
+            {
+                throw new Exception($"{id} owner is not found");
+            }
+            changeOwner.Name = updatedOwner.Name;
+            changeOwner.Surname = updatedOwner.Surname; 
+            changeOwner.Type = updatedOwner.Type;   
+            changeOwner.Statement = updatedOwner.Statement; 
+            return Ok(ownerList);
+
+        }
+    }
+}
---
+++ Owner.API/Owner.API/Middlewares/ExceptionMiddleware.cs
@@ -0,0 +1,70 @@
+﻿using Owner.API.Middlewares;
+using System.Net;
+using System.Text.Json;
+
+namespace Owner.API.Middlewares
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ExceptionMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+
+
+            try
+            {
+                await _next(httpContext);
+            }
+            catch (Exception exception)
+            {
+
+
+                await HandleExceptionAsync(httpContext, exception);
+
+
+            }
+
+        }
+
+
+
+        private static Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            HttpStatusCode status;
+            var stackTrace = String.Empty;
+            string message;
+            var exceptionType = exception.GetType();
+
+            message = exception.Message;
+            status = HttpStatusCode.BadRequest;
+            stackTrace = exception.StackTrace;
+
+
+            var exceptionResult = JsonSerializer.Serialize(new
+            {
+                statusCode = status,
+                error = message,
+                status = "This is an error message from Middleware.",
+                stackTrace,
+                
+
+            });
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)status;
+            return context.Response.WriteAsync(exceptionResult);
+        }
+    }
+
+}
+    static public class ExceptionMiddlewareExtension { 
+        static public IApplicationBuilder UseExceptionMiddleware(this IApplicationBuilder builder) { 
+            return builder.UseMiddleware<ExceptionMiddleware>();
+        } 
+    }
+
